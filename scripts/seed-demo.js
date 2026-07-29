76dad830db130ff2fb4cc9be032b777bc9ff825f
The-DarK-Knight-2K <ranujajayawardena@gmail.com>
2026-07-29 12:24:15 +0530

    Restore files from UTF-16 corruption (actual clean version)

--- scripts/seed-demo.js
+++ scripts/seed-demo.js
@@ -243,7 +243,3 @@ const runSeed = async () => {
 };
 
 runSeed();
-/ /   t r i g g e r   r e d e p l o y  
- / /   t r i g g e r   r e d e p l o y   2  
- / /   t r i g g e r   r e d e p l o y   3  
- 
